7aa78366151fe819b3b2ae75fc8fc9d9f1f7c07f
Veera Prasad <veeraprasad.koduri@gmail.com>
2026-03-07 21:55:40 +0530

    Delete frontend/bikes-rental-system directory

--- frontend/bikes-rental-system/src/components/Admin/adminPages/NoPage.jsx
+++
@@ -1,9 +0,0 @@
-const NoPage = () => {
-  return (
-    <div>
-      <h2>Oops! this page does not exist!</h2>
-    </div>
-  );
-};
-
-export default NoPage;
--- frontend/bikes-rental-system/src/components/Client/Pages/ClientDashboard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ClientDashboard() {
-  return (
-    <div>
-      <h1>This is the client dashboard</h1>
-    </div>
-  );
-}
