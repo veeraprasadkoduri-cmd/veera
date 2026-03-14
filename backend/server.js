a8ade0c68c711c9f28e103a0e255520bb2a22cb7
Veera Prasad <veeraprasad.koduri@gmail.com>
2026-03-14 15:38:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,4 @@
-// backend/server.js
-const express = require('express');  // import express
-const app = express();                // create app
-const PORT = process.env.PORT || 3000;  // use environment port or default 3000
-
-// Optional test route
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-});
-
-// Start server on 0.0.0.0 so Docker can expose it
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+app.use(express.static('../frontend'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/index.html'));
 });
